3889233cc962f40c816414cb07a22211b6abd7c3
Gennadiy Shvetsov <gennadiy.shvetsov@gmail.com>
2024-12-04 10:42:24 +0100

    change build artefact name

--- build.sh
+++ build.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 pip3 install --upgrade --target ./lib/ todoist_api_python
-zip -x ./env/**\* -x ./**/__pycache__/**\* -x build.sh -r ../getTasks.zip *
-echo '#!/usr/bin/env python3' | cat - ../getTasks.zip > ../getTasks.pyz
-chmod +x ../getTasks.pyz 
+zip -x ./env/**\* -x ./**/__pycache__/**\* -x build.sh -r ../getTodos.zip *
+echo '#!/usr/bin/env python3' | cat - ../getTodos.zip > ../getTodos.pyz
+chmod +x ../getTodos.pyz 
